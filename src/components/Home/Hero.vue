0198728d8240b77d2e277cb78110658a4d7cc9d1
haidarabdillah <113880800+haidarabdillah@users.noreply.github.com>
2025-11-30 22:33:19 +0700

    change minor

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -1,108 +1,210 @@
 <template>
-    <div class="max-w-[1920px] h-fit mx-auto flex flex-col">
-        <div class="w-full grid-cols-1 md:grid-cols-2 pb-[400px] md:pb-[600px] lg:pb-[835px] hidden md:grid">
-            <div class="pl-4 md:pl-8 lg:pl-[78px] w-full relative">
-                <img src="/src/assets/images/hero_light.png" alt="alt"
-                    class="absolute top-[-10vh] md:top-[-15vh] lg:top-[-20vh] left-[-20vh] md:left-[-30vh] lg:left-[-40vh] scale-[1.2] md:scale-[1.5] lg:scale-[2]">
-                <div class="flex flex-col absolute z-50 w-full justify-center items-start mt-8 md:mt-12 lg:mt-[82px]">
-                    <div class="h-[100px] md:h-[130px] lg:h-[170px] w-[300px] md:w-[400px] lg:w-[505px] rounded-br-full rounded-tr-full overflow-hidden">
-                    <img src="/src/assets/images/hero_image.jpeg" alt="alt" class="w-full h-full object-cover filter grayscale">
-                </div>
-                <p class="flex items-center text-white text-[40px] md:text-[60px] lg:text-[80px] font-bold leading-[46px] md:leading-[68px] lg:leading-[92px]">
-                        <span class="text-primary text-[18px] md:text-[24px] lg:text-[32px] font-bold leading-[20px] md:leading-[28px] lg:leading-[36px] mr-1">Nusatech <br /> Take the
-                            keys.</span>
-                        Digital
-                    </p>
-                    <p class="text-white text-[30px] md:text-[45px] lg:text-[58px] 2xl:text-[80px] font-bold leading-[34px] md:leading-[50px] lg:leading-[66px] 2xl:leading-[92px] mb-4 md:mb-6 lg:mb-[37px]">Blockchain Innovation For A
-                        Smarter
-                        Future
-                    </p>
-                    <div class="w-full max-w-[400px] md:max-w-[500px] lg:max-w-[606px] 2xl:max-w-[808px] h-auto md:h-[120px] lg:h-[172px] flex flex-col md:flex-row bg-white-005">
-                        <div class="w-full md:w-[130px] lg:w-[202px] 2xl:w-[202px] bg-primary flex justify-center p-4 md:p-6 lg:p-[40px]">
-                            <img src="/src/assets/images/logo_new.png" alt="alt" class="w-full max-w-[60px] md:max-w-none">
-                        </div>
-                        <div class="flex-1 md:w-[130px] lg:w-[202px] 2xl:w-[202px] flex flex-col justify-center px-2 md:px-4 lg:px-4 2xl:px-10 py-2 md:py-0">
-                            <p class="text-white text-[24px] md:text-[36px] lg:text-[48px] 2xl:text-[60px] font-bold">20%</p>
-                            <p class="text-xs md:text-sm lg:text-sm 2xl:text-[20px] text-secondary">trusted by companies</p>
-                        </div>
-                        <div class="flex-1 md:w-[130px] lg:w-[202px] 2xl:w-[202px] flex flex-col justify-center px-2 md:px-4 lg:px-4 2xl:px-10 py-2 md:py-0">
-                            <p class="text-white text-[24px] md:text-[36px] lg:text-[48px] 2xl:text-[60px] font-bold">13K</p>
-                            <p class="text-xs md:text-sm lg:text-sm 2xl:text-[20px] text-secondary">projects completed</p>
-                        </div>
-                        <div class="flex-1 md:w-[130px] lg:w-[202px] 2xl:w-[202px] flex flex-col justify-center px-2 md:px-4 lg:px-4 2xl:px-10 py-2 md:py-0">
-                            <p class="text-white text-[24px] md:text-[36px] lg:text-[48px] 2xl:text-[60px] font-bold">100%</p>
-                            <p class="text-xs md:text-sm lg:text-sm 2xl:text-[20px] text-secondary">Client
-                                Satisfaction</p>
-                        </div>
-                    </div>
-                </div>
-
-
+  <div class="max-w-[1920px] h-fit mx-auto flex flex-col">
+    <div
+      class="w-full grid-cols-1 md:grid-cols-2 pb-[400px] md:pb-[600px] lg:pb-[835px] hidden md:grid"
+    >
+      <div class="pl-4 md:pl-8 lg:pl-[78px] w-full relative">
+        <img
+          src="/src/assets/images/hero_light.png"
+          alt="alt"
+          class="absolute top-[-10vh] md:top-[-15vh] lg:top-[-20vh] left-[-20vh] md:left-[-30vh] lg:left-[-40vh] scale-[1.2] md:scale-[1.5] lg:scale-[2]"
+        />
+        <div
+          class="flex flex-col absolute z-50 w-full justify-center items-start mt-8 md:mt-12 lg:mt-[82px]"
+        >
+          <div
+            class="h-[100px] md:h-[130px] lg:h-[170px] w-[300px] md:w-[400px] lg:w-[505px] rounded-br-full rounded-tr-full overflow-hidden"
+          >
+            <img
+              src="/src/assets/images/hero_image.jpeg"
+              alt="alt"
+              class="w-full h-full object-cover filter grayscale"
+            />
+          </div>
+          <p
+            class="flex items-center text-white text-[40px] md:text-[60px] lg:text-[80px] font-bold leading-[46px] md:leading-[68px] lg:leading-[92px]"
+          >
+            <span
+              class="text-primary text-[18px] md:text-[24px] lg:text-[32px] font-bold leading-[20px] md:leading-[28px] lg:leading-[36px] mr-1"
+              >Nusatech <br />
+              Take the keys.</span
+            >
+            Digital
+          </p>
+          <p
+            class="text-white text-[30px] md:text-[45px] lg:text-[58px] 2xl:text-[80px] font-bold leading-[34px] md:leading-[50px] lg:leading-[66px] 2xl:leading-[92px] mb-4 md:mb-6 lg:mb-[37px]"
+          >
+            Blockchain Innovation For A Smarter Future
+          </p>
+          <div
+            class="w-full max-w-[400px] md:max-w-[500px] lg:max-w-[606px] 2xl:max-w-[808px] h-auto md:h-[120px] lg:h-[172px] flex flex-col md:flex-row bg-white-005"
+          >
+            <div
+              class="w-full md:w-[130px] lg:w-[202px] 2xl:w-[202px] bg-primary flex justify-center p-4 md:p-6 lg:p-[40px]"
+            >
+              <img
+                src="/src/assets/images/logo_new.png"
+                alt="alt"
+                class="w-full max-w-[60px] md:max-w-none"
+              />
             </div>
-            <div class="w-full relative">
-                <img src="/src/assets/images/hero_image_02.png" alt="alt"
-                    class="h-[500px] md:h-[700px] lg:h-[925px] absolute z-10 top-[-50px] md:top-[-70px] lg:top-[-90px] right-0 object-cover max-w-[calc(50vw)]">
-                <a href="" class="block">
-                    <img src="/src/assets/images/get_started_play.png" alt="alt" class="absolute z-20 top-[150px] md:top-[200px] lg:top-[300px] right-[100px] md:right-[200px] lg:right-[350px] w-[60px] md:w-[80px] lg:w-auto">
-                </a>
-                <div
-                    class="rounded-[0px_100px_100px_0px] md:rounded-[0px_200px_200px_0px] lg:rounded-[0px_300px_300px_0px] bg-white-017 px-4 md:px-6 lg:px-8 py-4 md:py-6 lg:py-[41px] absolute z-50 w-[280px] md:w-[400px] lg:w-[545px] xl:w-[480px] 2xl:w-[545px] top-[250px] md:top-[300px] xl:top-[508px]  2xl:top-[600px] left-0 2xl:left-20 max-w-[calc(50vw)]">
-                    <p class="text-sm md:text-base lg:text-lg text-secondary"><span class="text-white font-bold">Nusatech</span> with a solid
-                        team experience and trust from various clients, Nusatech is here as the best solution for
-                        innovative software and blockchain development.</p>
-                </div>
+            <div
+              class="flex-1 md:w-[130px] lg:w-[202px] 2xl:w-[202px] flex flex-col justify-center px-2 md:px-4 lg:px-4 2xl:px-10 py-2 md:py-0"
+            >
+              <p
+                class="text-white text-[24px] md:text-[36px] lg:text-[48px] 2xl:text-[60px] font-bold"
+              >
+                90%
+              </p>
+              <p class="text-xs md:text-sm lg:text-sm 2xl:text-[20px] text-secondary">
+                trusted by companies
+              </p>
             </div>
+            <div
+              class="flex-1 md:w-[130px] lg:w-[202px] 2xl:w-[202px] flex flex-col justify-center px-2 md:px-4 lg:px-4 2xl:px-10 py-2 md:py-0"
+            >
+              <p
+                class="text-white text-[24px] md:text-[36px] lg:text-[48px] 2xl:text-[60px] font-bold"
+              >
+                99+
+              </p>
+              <p class="text-xs md:text-sm lg:text-sm 2xl:text-[20px] text-secondary">
+                projects completed
+              </p>
+            </div>
+            <div
+              class="flex-1 md:w-[130px] lg:w-[202px] 2xl:w-[202px] flex flex-col justify-center px-2 md:px-4 lg:px-4 2xl:px-10 py-2 md:py-0"
+            >
+              <p
+                class="text-white text-[24px] md:text-[36px] lg:text-[48px] 2xl:text-[60px] font-bold"
+              >
+                95%%
+              </p>
+              <p class="text-xs md:text-sm lg:text-sm 2xl:text-[20px] text-secondary">
+                Client Satisfaction
+              </p>
+            </div>
+          </div>
         </div>
-        <div class="relative h-[500px] sm:h-[550px] md:hidden mb-[30px]">
-            <div class="absolute inset-0 -top-[50px] sm:-top-[70px]">
-                <img src="/src/assets/images/hero_image_02.png" alt="alt"
-                    class="h-[450px] sm:h-[525px] w-full object-cover">
-            </div>
-            <div class="absolute top-0 left-0 w-full flex flex-col space-y-3 sm:space-y-4 mt-3 sm:mt-4 px-4">
-                <p class="text-center text-primary text-[12px] sm:text-[14px] font-bold leading-[28px] sm:leading-[36px]">
-                    Nusatech Take the keys.
-                </p>
-                <p class="text-white text-[24px] sm:text-[30px] leading-[28px] sm:leading-[36px] text-center font-bold px-2">Digital Blockchain Innovation For A Smarter Future</p>
-                <div class="h-[70px] sm:h-[90px] rounded-full overflow-hidden mx-4 sm:mx-8">
-                    <img src="/src/assets/images/hero_image.jpeg" alt="alt" class="w-full h-full object-cover filter grayscale" style="object-position: 0px -250px;">
-                </div>
-                <p class="text-center text-xs sm:text-sm text-secondary px-4"> <span class="text-white">Nusatech</span> With a solid team experience and trust from various clients, Nusatech is here as the best solution for innovative software and blockchain development.</p>
-                <a href="" class="flex justify-center">
-                    <img src="/src/assets/images/get_started_play.png" alt="alt" class="w-[50px] sm:w-[60px]">
-                </a>
-                <div class="w-full h-auto sm:h-[101px] flex flex-col sm:flex-row bg-[#050505] mx-2">
-                        <div class="bg-primary flex justify-center px-4 py-4 sm:px-[23px] sm:py-[32px]">
-                            <img src="/src/assets/images/logo_new.png" alt="alt" class="h-[30px] sm:h-auto">
-                        </div>
-                        <div class="flex-1 flex flex-col justify-center px-2 sm:px-[8px] py-2 sm:py-[16px] border-t-[1px] border-b-[1px] sm:border-t-[1px] sm:border-b-[1px] border-grey">
-                            <p class="text-white text-lg sm:text-2xl font-bold text-center sm:text-left">20%</p>
-                            <p class="text-xs sm:text-sm text-secondary text-center sm:text-left">trusted by companies</p>
-                        </div>
-                        <div class="flex-1 flex flex-col justify-center px-2 sm:px-[8px] py-2 sm:py-[16px] border-[1px] border-r-0 sm:border-[1px] sm:border-r-0 border-grey">
-                            <p class="text-white text-lg sm:text-2xl font-bold text-center sm:text-left">13K</p>
-                            <p class="text-xs sm:text-sm text-secondary text-center sm:text-left">projects completed</p>
-                        </div>
-                        <div class="flex-1 flex flex-col justify-center px-2 sm:px-[8px] py-2 sm:py-[16px] border-[1px] sm:border-[1px] border-grey">
-                            <p class="text-white text-lg sm:text-2xl font-bold text-center sm:text-left">100%</p>
-                            <p class="text-xs sm:text-sm text-secondary text-center sm:text-left">Client
-                                Satisfaction</p>
-                        </div>
-                    </div>
-            </div>
+      </div>
+      <div class="w-full relative">
+        <img
+          src="/src/assets/images/hero_image_02.png"
+          alt="alt"
+          class="h-[500px] md:h-[700px] lg:h-[925px] absolute z-10 top-[-50px] md:top-[-70px] lg:top-[-90px] right-0 object-cover max-w-[calc(50vw)]"
+        />
+        <a href="" class="block">
+          <img
+            src="/src/assets/images/get_started_play.png"
+            alt="alt"
+            class="absolute z-20 top-[150px] md:top-[200px] lg:top-[300px] right-[100px] md:right-[200px] lg:right-[350px] w-[60px] md:w-[80px] lg:w-auto"
+          />
+        </a>
+        <div
+          class="rounded-[0px_100px_100px_0px] md:rounded-[0px_200px_200px_0px] lg:rounded-[0px_300px_300px_0px] bg-white-017 px-4 md:px-6 lg:px-8 py-4 md:py-6 lg:py-[41px] absolute z-50 w-[280px] md:w-[400px] lg:w-[545px] xl:w-[480px] 2xl:w-[545px] top-[250px] md:top-[300px] xl:top-[508px] 2xl:top-[600px] left-0 2xl:left-20 max-w-[calc(50vw)]"
+        >
+          <p class="text-sm md:text-base lg:text-lg text-secondary">
+            <span class="text-white font-bold">Nusatech</span> with a solid team
+            experience and trust from various clients, Nusatech is here as the best
+            solution for innovative software and blockchain development.
+          </p>
         </div>
-
+      </div>
     </div>
+    <div class="relative h-[500px] sm:h-[550px] md:hidden mb-[30px]">
+      <div class="absolute inset-0 -top-[50px] sm:-top-[70px]">
+        <img
+          src="/src/assets/images/hero_image_02.png"
+          alt="alt"
+          class="h-[450px] sm:h-[525px] w-full object-cover"
+        />
+      </div>
+      <div
+        class="absolute top-0 left-0 w-full flex flex-col space-y-3 sm:space-y-4 mt-3 sm:mt-4 px-4"
+      >
+        <p
+          class="text-center text-primary text-[12px] sm:text-[14px] font-bold leading-[28px] sm:leading-[36px]"
+        >
+          Nusatech Take the keys.
+        </p>
+        <p
+          class="text-white text-[24px] sm:text-[30px] leading-[28px] sm:leading-[36px] text-center font-bold px-2"
+        >
+          Digital Blockchain Innovation For A Smarter Future
+        </p>
+        <div class="h-[70px] sm:h-[90px] rounded-full overflow-hidden mx-4 sm:mx-8">
+          <img
+            src="/src/assets/images/hero_image.jpeg"
+            alt="alt"
+            class="w-full h-full object-cover filter grayscale"
+            style="object-position: 0px -250px"
+          />
+        </div>
+        <p class="text-center text-xs sm:text-sm text-secondary px-4">
+          <span class="text-white">Nusatech</span> With a solid team experience and trust
+          from various clients, Nusatech is here as the best solution for innovative
+          software and blockchain development.
+        </p>
+        <a href="" class="flex justify-center">
+          <img
+            src="/src/assets/images/get_started_play.png"
+            alt="alt"
+            class="w-[50px] sm:w-[60px]"
+          />
+        </a>
+        <div
+          class="w-full h-auto sm:h-[101px] flex flex-col sm:flex-row bg-[#050505] mx-2"
+        >
+          <div class="bg-primary flex justify-center px-4 py-4 sm:px-[23px] sm:py-[32px]">
+            <img
+              src="/src/assets/images/logo_new.png"
+              alt="alt"
+              class="h-[30px] sm:h-auto"
+            />
+          </div>
+          <div
+            class="flex-1 flex flex-col justify-center px-2 sm:px-[8px] py-2 sm:py-[16px] border-t-[1px] border-b-[1px] sm:border-t-[1px] sm:border-b-[1px] border-grey"
+          >
+            <p class="text-white text-lg sm:text-2xl font-bold text-center sm:text-left">
+              90%
+            </p>
+            <p class="text-xs sm:text-sm text-secondary text-center sm:text-left">
+              trusted by companies
+            </p>
+          </div>
+          <div
+            class="flex-1 flex flex-col justify-center px-2 sm:px-[8px] py-2 sm:py-[16px] border-[1px] border-r-0 sm:border-[1px] sm:border-r-0 border-grey"
+          >
+            <p class="text-white text-lg sm:text-2xl font-bold text-center sm:text-left">
+              99+
+            </p>
+            <p class="text-xs sm:text-sm text-secondary text-center sm:text-left">
+              projects completed
+            </p>
+          </div>
+          <div
+            class="flex-1 flex flex-col justify-center px-2 sm:px-[8px] py-2 sm:py-[16px] border-[1px] sm:border-[1px] border-grey"
+          >
+            <p class="text-white text-lg sm:text-2xl font-bold text-center sm:text-left">
+              100%
+            </p>
+            <p class="text-xs sm:text-sm text-secondary text-center sm:text-left">
+              Client Satisfaction
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Hero',
-    data() {
-        return {
-        };
-    },
-    mounted() {
-    }
+  name: "Hero",
+  data() {
+    return {};
+  },
+  mounted() {},
 };
 </script>
 
